d32f00dea1fb32b945b561e4104ad5b1d56c6351
Kajal Kolhe <kajal.kolhe2@outlook.com>
2024-07-26 11:20:04 +0530

    try catch demo

---
+++ try-catch.js
@@ -0,0 +1,30 @@
+try {
+  let a = 10;
+  let b = 0;
+  if (b === 0) throw new Error("Division by zero");
+  console.log(a / b);
+} catch (error) {
+  console.error(error.message);
+} finally {
+  console.log("This will run regardless of the error.");
+}
+
+function validateNumber(num) {
+  if (typeof num !== "number") {
+    throw new Error("Invalid argument, number expected");
+  } else {
+    console.log(num);
+  }
+  return num;
+}
+
+try {
+  validateNumber("a");
+} catch (error) {
+  console.error(error.message);
+}
+try {
+  validateNumber(12);
+} catch (error) {
+  console.error(error.message);
+}
