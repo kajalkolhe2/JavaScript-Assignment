df002468d1599fbc170829ab4b19e71e4dea999d
Kajal Kolhe <kajal.kolhe2@outlook.com>
2024-07-26 10:54:40 +0530

    variable demo

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "John";
+let age = 30;
+let isStudent = true;
+let height = null;
+let weight;
+let person = { firstName: "Jane", lastName: "Doe" };
+let colors = ["red", "green", "blue"];
+
+console.log(name, age, isStudent, height, weight, person, colors);
